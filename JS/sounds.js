7fc8bb542c5f7e22a4b79fb58de47ccb382779b1
cc042 <youssefkhalifa042@gmail.com>
2025-08-15 04:43:05 +0300

    Delete JS directory

--- JS/sounds.js
+++
@@ -1,6 +0,0 @@
-let currentAudio = null; // To track the currently playing audio
-let currentlyPlayingButton = null; // To track the currently active play button
-
-function playSurah(url, clickedButton) { }
-
-function pauseSurah() { }
